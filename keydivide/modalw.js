3525feff1b366dd0ac5f5b2e4cc5421eddf82102
Neyman39 <cacaoboards@mail.ru>
2024-06-22 00:47:17 +0300

    newrev

---
+++ keydivide/modalw.js
@@ -0,0 +1,16 @@
+const modalContainer = document.querySelector('.modal-container');
+const modalButton = document.querySelector('.my_orders');
+const modalClose = document.querySelector('.modal-close');
+
+
+modalButton.addEventListener('click', () => {
+  modalContainer.classList.add('modal-open');
+});
+modalClose.addEventListener('click', () => {
+  modalContainer.classList.remove('modal-open');
+});
+modalContainer.addEventListener('click', (event) => {
+  if (event.target === modalContainer) {
+    modalContainer.classList.remove('modal-open');
+  }
+});
