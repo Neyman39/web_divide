0e051fbbbb8a6170d89dfd9480acc13761420251
Neyman39 <cacaoboards@mail.ru>
2024-06-22 16:30:24 +0700

    new buttons

--- keydivide/js-style.js
+++ keydivide/js-style.js
@@ -1,54 +1,54 @@
 function chco1() {
-    document.getElementById("100").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("60").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("40").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("80").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("100").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("60").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("40").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("80").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco2() {
-    document.getElementById("80").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("60").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("40").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("100").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("80").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("60").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("40").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("100").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco3() {
-    document.getElementById("60").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("80").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("40").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("100").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("60").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("80").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("40").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("100").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco4() {
-    document.getElementById("40").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("80").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("60").style.border = '2px solid rgb(96, 96, 96)'
-    document.getElementById("100").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("40").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("80").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("60").style.border = '1px solid rgb(96, 96, 96)'
+    document.getElementById("100").style.border = '1px solid rgb(96, 96, 96)'
 }
 
 // film мод
 function chco5() {
-    document.getElementById("without").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("with").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("without").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("with").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco6() {
-    document.getElementById("with").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("without").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("with").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("without").style.border = '1px solid rgb(96, 96, 96)'
 }
 
 // тип можтажа переклюяателей
 function chco7() {
-    document.getElementById("paika").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("hots").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("paika").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("hots").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco8() {
-    document.getElementById("hots").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("paika").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("hots").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("paika").style.border = '1px solid rgb(96, 96, 96)'
 }
 
 // тип диодов
 function chco9() {
-    document.getElementById("smd").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("dip").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("smd").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("dip").style.border = '1px solid rgb(96, 96, 96)'
 }
 function chco10() {
-    document.getElementById("dip").style.border = '2px solid rgb(255, 122, 105)'
-    document.getElementById("smd").style.border = '2px solid rgb(96, 96, 96)'
+    document.getElementById("dip").style.border = '1px solid rgb(255, 122, 105)'
+    document.getElementById("smd").style.border = '1px solid rgb(96, 96, 96)'
 }
